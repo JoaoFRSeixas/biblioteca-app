ef83cab1a031e51a30fadfb2cd1760ab4a2f2651
João Seixas <joao.rodrigues@instaleap.io>
2024-08-02 15:43:55 -0300

    Create React APP

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AddBook from './components/AddBook';
+import EditBook from './components/EditBook';
+import Books from './components/Books'; // Certifique-se de ter criado este componente
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-book" element={<AddBook />} />
+        <Route path="/edit-book/:id" element={<EditBook />} />
+        <Route path="/books" element={<Books />} /> {/* Nova rota */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
